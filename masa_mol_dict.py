b221c802f981807b4c1902596a9ee7b6f6ccb9c0
JGabela <josemiguelgabela101@gmail.com>
2021-09-03 14:32:52 -0500

    1.0

---
+++ masa_mol_dict.py
@@ -0,0 +1,21 @@
+alkali = {
+    "Litio" : 6.94,
+    "Sodio" : 22.99,
+    "Potasio" : 39.10,
+    "Cesio" : 132.91,
+    "Rubidio" : 85.47,
+    "Francio" : 223.00
+}
+
+alkalinos = {
+    "Calcio" : 40.80,
+    "Bario" : 137.33,
+    "Berilio" : 9.01,
+    "Magnesio" : 24.31,
+    "Estroncio" : 87.62,
+    "Radio" : 226.00
+}
+
+transicion = {
+    "Hierro" : 27
+}
