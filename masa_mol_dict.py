c6aa8b39828c80379a0e49b0b494b3c9b4b924c4
JGabela <josemiguelgabela101@gmail.com>
2021-09-04 17:31:05 -0500

    ...

--- masa_mol_dict.py
+++ masa_mol_dict.py
@@ -1,4 +1,5 @@
 alkali = {
+
     "Litio" : 6.94,
     "Sodio" : 22.99,
     "Potasio" : 39.10,
@@ -8,6 +9,7 @@ alkali = {
 }
 
 alkalinos = {
+
     "Calcio" : 40.80,
     "Bario" : 137.33,
     "Berilio" : 9.01,
@@ -17,5 +19,146 @@ alkalinos = {
 }
 
 transicion = {
-    "Hierro" : 27
+
+    "Escandio" : 1,
+    "Titanio" : 1,
+    "Vanadio" : 1,
+    "Cromo" : 1,
+    "Manganeso" : 1,
+    "Hierro" : 55.85,
+    "Cobalto" : 1,
+    "Niquel" : 1, 
+    "Cobre" : 1,
+    "Zinc" : 1,
+    "Itrio" : 1,
+    "Zirconio" : 1,
+    "Niobio" : 1,
+    "Molibdeno" : 1,
+    "Tecnecio" : 1,
+    "Rutenio" : 1,
+    "Rodio" : 1,
+    "Paladio" : 1,
+    "Plata" : 1,
+    "Cadmio" : 1,
+    "Lutecio" : 1,
+    "Hafnio" : 1,
+    "Tantalio" : 1,
+    "Wolframio" : 1,
+    "Renio" : 1,
+    "Osmio" : 1,
+    "Iridio" : 1,
+    "Platino" : 1,
+    "Oro" : 1,
+    "Mercurio" : 1,
+    "Laurencio" : 1,
+    "Rutherfordio" : 1,
+    "Dubnio" : 1,
+    "Seaborgio" : 1,
+    "Bohrio" : 1,
+    "Hassio" : 1,
+    "Meitnerio" : 1,
+    "Darmstadtio" : 1,
+    "Roentgenio" : 1,
+    "Copernico" : 1
+
+}
+
+halogenos = {
+
+    "Bromo" : 1,
+    "Fluor" : 1,
+    "Cloro" : 1,
+    "Yodo" : 1,
+    "Astato" : 1,
+    "Teneso" : 1
+
+}
+
+gases_nobles = {
+
+    "Helio" : 1,
+    "Neon" : 1,
+    "Argon" : 1,
+    "Kripton" : 1,
+    'Xenon' : 1,
+    "Radon" : 1,
+    "Oganeson" : 1
+
+}
+
+no_metales = {
+
+    "Hidrogeno" : 1,
+    "Carbono" : 1,
+    "Nitrogeno" : 1,
+    "Oxigeno" : 1,
+    "Fosforo" : 1,
+    "Azufre" : 1,
+    "Selenio" : 1
+
+}
+
+otros_metales = {
+
+    "Aluminio" : 1,
+    "Galio" : 1,
+    "Indio" : 1, 
+    "Estaño" : 1,
+    "Talio" : 1,
+    "Plomo" : 1,
+    "Bismuto" : 1,
+    "Nihonio" : 1,
+    "Flerovio" : 1,
+    "Moscovio" : 1,
+    "Livermorio" : 1
+
+
+}
+
+metaloides = {
+
+    "Boro" : 1,
+    "Silicio" : 1,
+    "Germanio" : 1,
+    "Arsenico" : 1, 
+    "Antimonio" : 1,
+    "Teluro" : 1,
+    "Polonio" : 1
+
+}
+
+latanidos = {
+
+    "lantano" : 1,
+    "Cerio" : 1,
+    "Prasedomio" : 1,
+    "Prometio" : 1,
+    "Samario" : 1,
+    "Gadolinio" : 1,
+    "Terbio" : 1,
+    "Disprosio" : 1, 
+    "Holmio" : 1,
+    "Erbio" : 1,
+    "Tulio" : 1, 
+    "Iterbio" : 1
+
+}
+
+Actinidos = {
+
+    "Actindos" : 1,
+    "Torio" : 1,
+    "Protactnio" : 1,
+    "Uranio" : 1,
+    "Neptunio" : 1,
+    "Plutonio" : 1,
+    "Americo" : 1,
+    "Curio" : 1,
+    "Berquelio" : 1,
+    "Californio" : 1,
+    "Einstenio" : 1,
+    "Fermio" : 1,
+    "Mendelevio" : 1,
+    "Nobelio" : 1
+
 }
